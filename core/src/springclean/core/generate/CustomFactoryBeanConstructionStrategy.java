0d8a53e262544d864c95d23bab91fb8a7386ffd9
David Denton <denton.david@gmail.com>
2010-01-31 19:23:12 +0000

    dirty - feature work

--- core/src/springclean/core/generate/CustomFactoryBeanConstructionStrategy.java
+++ core/src/springclean/core/generate/CustomFactoryBeanConstructionStrategy.java
@@ -1,14 +1,12 @@
 package springclean.core.generate;
 
-import static com.google.common.collect.Lists.newArrayList;
 import org.daisychain.source.Instance;
 import org.daisychain.source.body.AssignableStatement;
 import springclean.core.domain.Bean;
-import springclean.core.domain.Property;
+import static springclean.core.domain.Property.Util.propertyInjectedDependencies;
 import springclean.core.domain.Reference;
 import static springclean.core.generate.ContextElement.DependencyExtractor.allDependenciesOf;
 
-import java.util.List;
 import java.util.Set;
 
 public class CustomFactoryBeanConstructionStrategy implements ConstructionStrategy {
@@ -21,16 +19,12 @@ public class CustomFactoryBeanConstructionStrategy implements ConstructionStrate
     }
 
     public AssignableStatement asStatement() {
-        return new FactoryMethodInvocation(bean, CustomFactoryBeanConstructionStrategy.this.reference);
+        return new FactoryMethodInvocation(bean, reference);
 
     }
 
     public Set<Instance> dependencies() {
-        final List<ContextElement> injectedDependencies = newArrayList();
-        for (Property property : bean.setterDependencies()) {
-            injectedDependencies.add(property.referencedObject().asContextElement(bean.setter(property).parameters().get(0).instance.aClass));
-        }
-        return allDependenciesOf(injectedDependencies);
+        return allDependenciesOf(propertyInjectedDependencies(bean));
     }
 
 }
