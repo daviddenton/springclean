25c0ee8b8443296d41ccb4bf223a61d112f7a28e
David Denton <denton.david@gmail.com>
2010-01-31 11:12:49 +0000

    dirty - feature work

--- core/src/springclean/core/generate/ConstructorInvocation.java
+++ core/src/springclean/core/generate/ConstructorInvocation.java
@@ -1,34 +1,29 @@
 package springclean.core.generate;
 
-import static com.google.common.collect.Lists.newArrayList;
 import org.daisychain.source.AClass;
 import static org.daisychain.source.HasImports.ImportExtractor.extractImportsFrom;
 import org.daisychain.source.body.AssignableStatement;
 import org.daisychain.source.util.IndentingStringWriter;
 import springclean.core.domain.Bean;
+import static springclean.core.domain.Bean.Util.asStatements;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 public class ConstructorInvocation implements AssignableStatement {
 
     private final Bean bean;
-    private final List<AssignableStatement> constructorStatements = newArrayList();
 
     public ConstructorInvocation(Bean bean) {
         this.bean = bean;
-        for (int i = 0; i < bean.constructorArgs().size(); i++) {
-            constructorStatements.add(bean.constructorArgs().get(i).referencedObject().asContextElement(bean.constructor().parameters().get(i).instance.aClass).asStatement());
-        }
     }
 
     public Set<AClass> getImports() {
-        return extractImportsFrom(constructorStatements);
+        return extractImportsFrom(asStatements(bean));
     }
 
     public void appendSource(IndentingStringWriter writer) throws IOException {
-        bean.clazz().instantiate(constructorStatements).appendSource(writer);
+        bean.clazz().instantiate(asStatements(bean)).appendSource(writer);
     }
 
 }
