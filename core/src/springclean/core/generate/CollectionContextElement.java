3c72843e047f8f341d08668fcac33d19ab409bf4
David Denton <denton.david@gmail.com>
2010-01-31 21:25:10 +0000

    dirty - feature work

--- core/src/springclean/core/generate/CollectionContextElement.java
+++ core/src/springclean/core/generate/CollectionContextElement.java
@@ -1,10 +1,20 @@
 package springclean.core.generate;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
-import org.daisychain.source.Instance;
+import org.daisychain.source.*;
+import static org.daisychain.source.HasImports.ImportExtractor.extractImportsFrom;
 import org.daisychain.source.body.AssignableStatement;
+import org.daisychain.source.util.IndentingStringWriter;
+import static org.daisychain.source.util.ListAppender.generateSource;
+import static org.daisychain.source.util.ListAppender.loop;
 import springclean.core.domain.BeanCollection;
+import springclean.core.domain.SpringManagedObject;
 
+import java.io.IOException;
+import java.util.Collections;
+import static java.util.Collections.singletonList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -20,6 +30,28 @@ public class CollectionContextElement implements ConstructionStrategy {
     }
 
     public AssignableStatement asStatement() {
-        return new CollectionConstruction(beanCollection);
+        Method addMethod = new MethodFinder(beanCollection.clazz()).method("add", 1);
+        final List<AssignableStatement> postConstructionStatements = newArrayList();
+
+        for (SpringManagedObject member : beanCollection.members()) {
+            AssignableStatement memberStatement = member.asConstructionStrategy(new ExistingClass(Object.class)).asStatement();
+            postConstructionStatements.add(addMethod.call(singletonList(memberStatement)));
+        }
+
+        return new AssignableStatement() {
+            public Set<AClass> getImports() {
+                return extractImportsFrom(singletonList(beanCollection.clazz()), postConstructionStatements);
+            }
+
+            public void appendSource(IndentingStringWriter writer) throws IOException {
+                beanCollection.clazz().instantiate(Collections.EMPTY_LIST).appendSource(writer);
+                loop(postConstructionStatements)
+                        .withPrefix(new InitializerBlockStart())
+                        .andForEach(generateSource()).seperatedBy(";\n")
+                        .withSuffix(new InitializerBlockEnd())
+                        .to(writer);
+            }
+        };
+
     }
 }
