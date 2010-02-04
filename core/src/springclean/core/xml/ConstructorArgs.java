56981704d3de8e720994b52486820f6f050fc807
David Denton <denton.david@gmail.com>
2010-02-04 23:00:36 +0000

    dirty - feature work

--- core/src/springclean/core/xml/ConstructorArgs.java
+++ core/src/springclean/core/xml/ConstructorArgs.java
@@ -1,16 +1,19 @@
 package springclean.core.xml;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
 import springclean.core.domain.ConstructorArg;
 
 import java.util.List;
+import java.util.Set;
 
 public class ConstructorArgs {
     private final List<ConstructorArg> localConstructorArgs;
 
     public ConstructorArgs(List<ConstructorArg> localConstructorArgs) throws IllegalConstructorArgs {
         this.localConstructorArgs = localConstructorArgs;
-        verifyIndexes(localConstructorArgs);
+        verifyIndexedIntegity(localConstructorArgs);
+        verifyIndexDuplicates(localConstructorArgs);
     }
 
     public ConstructorArgs(ConstructorArg... localConstructorArgs) throws IllegalConstructorArgs {
@@ -25,7 +28,7 @@ public class ConstructorArgs {
         return localConstructorArgs;
     }
 
-    private void verifyIndexes(List<ConstructorArg> constructorArgs) throws IllegalConstructorArgs {
+    private void verifyIndexedIntegity(List<ConstructorArg> constructorArgs) throws IllegalConstructorArgs {
         int indexedCount = 0;
         for (ConstructorArg constructorArg : constructorArgs) {
             if (constructorArg.isIndexed()) indexedCount++;
@@ -33,6 +36,21 @@ public class ConstructorArgs {
         if (indexedCount != 0 && indexedCount != constructorArgs.size()) {
             throw new IllegalConstructorArgs();
         }
+    }
+
+    private void verifyIndexDuplicates(List<ConstructorArg> constructorArgs) throws IllegalConstructorArgs {
+        Set<Integer> indexes = indexesFrom(constructorArgs);
+        if (!indexes.isEmpty() && indexes.size() != constructorArgs.size()) {
+            throw new IllegalConstructorArgs();
+        }
+    }
+
+    private Set<Integer> indexesFrom(List<ConstructorArg> constructorArgs) {
+        Set<Integer> indexes = newHashSet();
+        for (ConstructorArg constructorArg : constructorArgs) {
+            if (constructorArg.isIndexed()) indexes.add(constructorArg.index());
+        }
+        return indexes;
     }
 
     public class IllegalConstructorArgs extends Exception {
