00a1dd917309ab8f8b10e4bbc0d34fe83d010972
David Denton <denton.david@gmail.com>
2010-02-16 23:16:20 +0000

    dirty - collections

--- core/src/springclean/core/xml/XmlBeanMapEntry.java
+++ core/src/springclean/core/xml/XmlBeanMapEntry.java
@@ -1,13 +1,22 @@
 package springclean.core.xml;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
 import nu.xom.Element;
-import org.daisychain.source.AClass;
+import org.daisychain.source.*;
+import static org.daisychain.source.ExistingClass.existingClass;
+import org.daisychain.source.body.AssignableStatement;
 import springclean.core.domain.ApplicationContext;
 import springclean.core.domain.BeanMapEntry;
 import springclean.core.domain.SpringManagedObject;
 import springclean.core.generate.ConstructionStrategy;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class XmlBeanMapEntry extends AbstractElementWrapper implements BeanMapEntry {
+    private static final ExistingClass MAP_CLASS = existingClass(HashMap.class);
+
     public XmlBeanMapEntry(Element beanNode, ApplicationContext applicationContext) {
         super(beanNode, applicationContext);
     }
@@ -21,6 +30,18 @@ public class XmlBeanMapEntry extends AbstractElementWrapper implements BeanMapEn
     }
 
     public ConstructionStrategy asConstructionStrategy(AClass aClass) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new ConstructionStrategy() {
+            public AssignableStatement asStatement() {
+                Method putMethod = new MethodFinder<ExistingMethod>(MAP_CLASS).method("put", 2);
+                AssignableStatement keyStatement = key().asConstructionStrategy(existingClass(Object.class)).asStatement();
+                AssignableStatement valueStatement = value().asConstructionStrategy(existingClass(Object.class)).asStatement();
+                return putMethod.call(newArrayList(keyStatement, valueStatement));
+            }
+
+            public Set<Instance> dependencies() {
+                // will not work
+                return newHashSet();
+            }
+        };
     }
 }
